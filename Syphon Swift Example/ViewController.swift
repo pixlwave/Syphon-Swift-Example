6fb9b43712a57a8f287fc33734d6805b65522f8b
Doug <git@digitalfx.me.uk>
2016-09-11 10:38:46 +0100

    Convert to Swift 3.0

--- Syphon Swift Example/ViewController.swift
+++ Syphon Swift Example/ViewController.swift
@@ -3,7 +3,7 @@ import AVFoundation
 
 class ViewController: NSViewController {
     
-    var displayTimer: NSTimer?
+    var displayTimer: Timer?
     var context: NSOpenGLContext?
     
     var player: AVPlayer?
@@ -15,7 +15,7 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        displayTimer = NSTimer.scheduledTimerWithTimeInterval(1 / 60, target: self, selector: "screenRefresh", userInfo: nil, repeats: true)
+        displayTimer = Timer.scheduledTimer(timeInterval: 1 / 60, target: self, selector: #selector(screenRefresh), userInfo: nil, repeats: true)
         
         let contextAttributes: [NSOpenGLPixelFormatAttribute] = [
             NSOpenGLPixelFormatAttribute(NSOpenGLPFADoubleBuffer),
@@ -23,32 +23,32 @@ class ViewController: NSViewController {
             NSOpenGLPixelFormatAttribute(0)
         ]
         
-        context = NSOpenGLContext(format: NSOpenGLPixelFormat(attributes: contextAttributes)!, shareContext: nil)
+        context = NSOpenGLContext(format: NSOpenGLPixelFormat(attributes: contextAttributes)!, share: nil)
         context?.makeCurrentContext()
         
         if texture == 0 { glGenTextures(1, &texture) }
         
-        syphonServer = SyphonServer(name: "Video", context: context!.CGLContextObj, options: nil)
+        syphonServer = SyphonServer(name: "Video", context: context!.cglContextObj, options: nil)
         
-        player = AVPlayer(URL: NSBundle.mainBundle().URLForResource("video", withExtension: "mov")!)
+        player = AVPlayer(url: Bundle.main.url(forResource: "video", withExtension: "mov")!)
         
-        let bufferAttributes: [String: AnyObject] = [
-            String(kCVPixelBufferPixelFormatTypeKey): Int(kCVPixelFormatType_32BGRA),
+        let bufferAttributes: [String: Any] = [
+            String(kCVPixelBufferPixelFormatTypeKey): Int(kCVPixelFormatType_32BGRA)
             String(kCVPixelBufferIOSurfacePropertiesKey): [String: AnyObject](),
             String(kCVPixelBufferOpenGLCompatibilityKey): true
         ]
         
         videoOutput = AVPlayerItemVideoOutput(pixelBufferAttributes: bufferAttributes)
         videoOutput.suppressesPlayerRendering = true
-        player?.currentItem?.addOutput(videoOutput)
+        player?.currentItem?.add(videoOutput)
         
         player?.play()
     }
     
     func screenRefresh() {
-        let itemTime = videoOutput.itemTimeForHostTime(CACurrentMediaTime())
-        if videoOutput.hasNewPixelBufferForItemTime(itemTime) {
-            if let pixelBuffer = videoOutput.copyPixelBufferForItemTime(itemTime, itemTimeForDisplay: nil) {
+        let itemTime = videoOutput.itemTime(forHostTime: CACurrentMediaTime())
+        if videoOutput.hasNewPixelBuffer(forItemTime: itemTime) {
+            if let pixelBuffer = videoOutput.copyPixelBuffer(forItemTime: itemTime, itemTimeForDisplay: nil) {
                 if let surface = CVPixelBufferGetIOSurface(pixelBuffer)?.takeUnretainedValue() {
                     
                     size = NSSize(width: IOSurfaceGetWidth(surface), height: IOSurfaceGetHeight(surface))
@@ -56,7 +56,7 @@ class ViewController: NSViewController {
                     context?.makeCurrentContext()
                     
                     glBindTexture(GLenum(GL_TEXTURE_RECTANGLE_EXT), texture)
-                    CGLTexImageIOSurface2D(context!.CGLContextObj, GLenum(GL_TEXTURE_RECTANGLE_EXT), GLenum(GL_RGBA), GLsizei(size.width), GLsizei(size.height), GLenum(GL_BGRA), GLenum(GL_UNSIGNED_INT_8_8_8_8_REV), surface, 0)
+                    CGLTexImageIOSurface2D(context!.cglContextObj!, GLenum(GL_TEXTURE_RECTANGLE_EXT), GLenum(GL_RGBA), GLsizei(size.width), GLsizei(size.height), GLenum(GL_BGRA), GLenum(GL_UNSIGNED_INT_8_8_8_8_REV), surface, 0)
                 }
             }
         }
@@ -64,7 +64,7 @@ class ViewController: NSViewController {
         syphonServer?.publishFrameTexture(texture, textureTarget: GLenum(GL_TEXTURE_RECTANGLE_EXT), imageRegion: NSRect(origin: CGPoint(x: 0, y: 0), size: size), textureDimensions: size, flipped: true)
     }
     
-    @IBAction func rewind(sender: AnyObject) {
-        player?.seekToTime(kCMTimeZero)
+    @IBAction func rewind(_ sender: AnyObject) {
+        player?.seek(to: kCMTimeZero)
     }
 }
